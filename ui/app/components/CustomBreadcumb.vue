a23b8317fe16316613d61575ded35e819bf33ebb
Jude mpoyo <mpoyojude0@gmail.com>
2026-02-14 15:56:33 +0200

    feat : create custom breadcumbs component

---
+++ ui/app/components/CustomBreadcumb.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+<template>
+  <div class="card flex mt-2 min-w-full">
+    <Breadcrumb :home="home" :model="items">
+      <template #item="{ item, props }">
+        <router-link
+          v-if="item.route"
+          v-slot="{ href, navigate }"
+          :to="item.route"
+          custom
+        >
+          <a :href="href" v-bind="props.action" @click="navigate">
+            <span :class="[item.icon, 'text-color']" />
+            <span class="text-primary font-semibold">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+          <span class="text-surface-700 text-sm dark:text-surface-0">{{
+            item.label
+          }}</span>
+        </a>
+      </template>
+    </Breadcrumb>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
---
+++ ui/app/composables/useBreadcumb.ts
@@ -0,0 +1,5 @@
+export const home = ref({
+    icon: "pi pi-home",
+    route: "/",
+});
+export const items = ref([{ label: "routes" }]);
